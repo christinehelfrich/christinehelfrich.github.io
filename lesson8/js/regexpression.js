d9b781cda5b48c50993ddf55aedad0d53642bae5
christinehelfrich <60027020+christinehelfrich@users.noreply.github.com>
2021-06-16 16:02:41 -1000

    Create regexpression.js

---
+++ lesson8/js/regexpression.js
@@ -0,0 +1,18 @@
+
+
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
